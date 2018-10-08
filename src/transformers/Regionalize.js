565cba9a5d7f41bb3fbc2d0c69749edd43500fb5
Kaden Thomas <thomas.kaden4@outlook.com>
2018-10-07 23:08:11 -0700

    Added numeric support to regionalization

--- src/transformers/Regionalize.js
+++ src/transformers/Regionalize.js
@@ -1,8 +1,29 @@
 const isAlpha = e => (e >= 'A' && e <= 'Z') || (e >= 'a' && e <= 'z');
+const isNumeric = e => (e >= '0' && e <= '9');
 
 export const regionalize = text => {
     let chars = Array.from(text);
-    let regional = chars.map((e, _) => isAlpha(e) ? `:regional_indicator_${e.toLowerCase()}:` : e);
+    let regional = chars.map((e, _) => {
+        if (isAlpha(e)) {
+            return `:regional_indicator_${e.toLowerCase()}:`;
+        } else if (isNumeric(e)) {
+            switch (e) {
+                case "0": return ":zero:";
+                case "1": return ":one:";
+                case "2": return ":two:";
+                case "3": return ":three:";
+                case "4": return ":four:";
+                case "5": return ":five:";
+                case "6": return ":six:";
+                case "7": return ":seven:";
+                case "8": return ":eight:";
+                case "9": return ":nine:";
+            }
+        } else {
+            return e;
+        }
+
+    });
     return regional.join(" ");
 };
 
