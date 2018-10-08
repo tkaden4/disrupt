da099ddb4fa9dea8d41f0f04bf0ae6f5b5644466
Kaden Thomas <thomas.kaden4@outlook.com>
2018-10-08 09:12:54 -0700

    Add character counter to output

--- src/components/Transformer.js
+++ src/components/Transformer.js
@@ -1,22 +1,41 @@
 import React from "react";
 import MessageBox from "./MessageBox";
+import classnames from "classnames";
+
+const MAX_CHARS = 2000;
 
 import "./Transformer.sass";
 
-export const TransformedView = ({ name, transformer, content, onChange }) =>
-    <div className="transformer">
+
+const MessageLengthIndicator = ({ length }) =>
+    <div className={classnames("message-indicator", { "bad": length <= 0, "good": length > 0 })} >
+        {length} chars left
+    </div>;
+
+export const TransformedView = ({ name, transformer, content, onChange }) => {
+    let output = transformer(content);
+    let out_len = output.length;
+    let out_diff = MAX_CHARS - out_len;
+    return <div className="transformer">
         <div className="title">{name}</div>
         <div className="content">
             <div className="sub">
-                <div className="title">Input</div>
+                <div className="title">
+                    Input
+                    {/* <MessageLengthIndicator length={MAX_CHARS - content.length} /> */}
+                </div>
                 <MessageBox content={content} onChange={onChange} />
             </div>
             <div className="sub">
-                <div className="title">Output</div>
-                <MessageBox content={transformer(content)} disabled />
+                <div className="title">
+                    Output
+                    <MessageLengthIndicator length={out_diff} />
+                </div>
+                <MessageBox content={output} disabled />
             </div>
         </div>
     </div>;
+}
 
 export class Transformer extends React.Component {
     constructor(props) {
