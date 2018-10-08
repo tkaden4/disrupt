01a14bce49f87c50a141a2ccd57d085078c48894
Kaden Thomas <thomas.kaden4@outlook.com>
2018-10-07 22:59:13 -0700

    Fix errors in translation

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -5,7 +5,7 @@ import "./MessageBox.sass";
 export const MessageBox = ({ content, placeholder, disabled, onChange }) =>
     <div className="message-box">
         <form>
-            <textarea placeholder={placeholder} disabled={disabled} onChange={e => { }} onInput={e => onChange(e.target.value)} value={content}>
+            <textarea placeholder={placeholder} onChange={e => { }} onInput={e => onChange(e.target.value)} value={content}>
             </textarea>
         </form>
     </div>;
--- src/transformers/Regionalize.js
+++ src/transformers/Regionalize.js
@@ -3,7 +3,7 @@ const isAlpha = e => (e >= 'A' && e <= 'Z') || (e >= 'a' && e <= 'z');
 export const regionalize = text => {
     let chars = Array.from(text);
     let regional = chars.map((e, _) => isAlpha(e) ? `:regional_indicator_${e.toLowerCase()}:` : e);
-    return regional.join("");
+    return regional.join(" ");
 };
 
 export default regionalize;
